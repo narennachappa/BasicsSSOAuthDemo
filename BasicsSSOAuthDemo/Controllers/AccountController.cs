4466d841bb4dc7ddc421ba8b7f37e1aad5d8b187
narennachappa <narennachappa@yahoo.com>
2020-04-05 14:30:04 +1000

    Add project files.

---
+++ BasicsSSOAuthDemo/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace BasicsSSOAuthDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            if (Request.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password, string returnUrl)
+        {
+            if (FormsAuthentication.Authenticate(username, password))
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+                if (!string.IsNullOrEmpty(returnUrl))
+                {
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login details");
+                ViewBag.ReturnUrl = returnUrl;
+                return View();
+            }
+        }
+    }
+}
---
+++ BasicsSSOAuthDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BasicsSSOAuthDemo.MvcApplication" Language="C#" %>
